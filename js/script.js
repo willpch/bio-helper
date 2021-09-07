a46f4b98be8838f1eff01dfc56ffe97089227a4e
willpch <lolplayerbr@gmail.com>
2021-09-06 21:12:55 -0300

    Add styles and more JS validation

--- js/script.js
+++ js/script.js
@@ -1,10 +1,33 @@
 var ul = document.querySelector('ul');
 var item = document.querySelector('li');
+var input = document.querySelector('#inp-num');
+
+function InputLength() {
+    return input.value.length;
+}
+
+input.addEventListener("keyup", function(event) {
+
+    if(event.keyCode === 13) {
+        event.preventDefault();
+        document.querySelector('.btn-add').click();
+    }
+});
+
+function addClick() {
+    if (InputLength() > 0) {
+        createList();
+    } else {
+        window.alert("Por favor, insira quantidade.");
+    }
+}
+
 
 function createList() {
     var select = document.querySelector('#sel');
     var val = select.options[select.selectedIndex].text;
-
+    var inpNum = document.querySelector('#inp-num').value;
+    console.log(inpNum);
     var li = document.createElement('li');
 
     li.appendChild(document.createTextNode(val));
@@ -15,6 +38,10 @@ function createList() {
     xBtn.appendChild(document.createTextNode("x"));
     li.appendChild(xBtn);
     xBtn.addEventListener("click", deleteListItem);
+
+    document.querySelector('#inp-num').value = "";
+
+
 
     function deleteListItem() {
         li.innerHTML = null;
